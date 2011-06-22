c9b2d13c45d94376540f191782a4004bdce341a3
Gerry Kroll <user@domain>
2011-06-22 20:28:05 +0000

    Improve hacking detection
    git-svn-id: https://svn.code.sf.net/p/phpgedview/svn/trunk@7043 8eba034c-2814-0410-b8e3-dd7cca3e1d4e

--- phpGedView/module.php
+++ phpGedView/module.php
@@ -38,84 +38,92 @@ define('PGV_MOD_V2', 3);
 // Sanitize all input
 $mod		= safe_REQUEST($_REQUEST, 'mod',		PGV_REGEX_MODNAME, '');
 $name		= safe_REQUEST($_REQUEST, 'name',		PGV_REGEX_MODNAME, '');
-$pgvaction	= safe_REQUEST($_REQUEST, 'pgvaction',	PGV_REGEX_MODNAME, '');
-$class		= safe_REQUEST($_REQUEST, 'class',		PGV_REGEX_MODNAME, '');
-$method		= safe_REQUEST($_REQUEST, 'method',		PGV_REGEX_MODNAME, '');
+$pgvaction	= safe_REQUEST($_REQUEST, 'pgvaction',	PGV_REGEX_MODNAME, 'index');
+$class		= safe_REQUEST($_REQUEST, 'class',		PGV_REGEX_MODNAME, $mod);
+$method		= safe_REQUEST($_REQUEST, 'method',		PGV_REGEX_MODNAME, 'main');
 if (empty($mod))		unset($mod);
 if (empty($name))		unset($name);
 if (empty($pgvaction))	unset($pgvaction);
 if (empty($class))		unset($class);
 if (empty($method))		unset($method);
 
-if(!isset($mod))
-{
+if(!isset($mod)) {
 	// PGV_MOD_NUKE
 	if (isset($name)) $mod = $name;
-	else $mod = 'index';
 }
-if(file_exists('modules/'.$mod.'.php'))
-{
-	$modinfo = parse_ini_file('modules/'.$mod.'.php', true);
-}
-// v2 modules
-elseif(file_exists("modules/{$mod}/pgv_version.php"))
-{
+
+$ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "";
+$log_msg = '';
+if ($mod == '' || !is_dir("modules/{$mod}")) {
+	$log_msg = 'Attempt to run non-existent module';	// No matching sub-directory
+} else if (file_exists("modules/{$mod}.php")) {
+	$modinfo = parse_ini_file("modules/{$mod}.php", true);
+} else if (file_exists("modules/{$mod}/pgv_version.php")) {
 	$modinfo = parse_ini_file("modules/{$mod}/pgv_version.php", true);
+} else {
+	$log_msg = 'Attempt to run non-existent module';	// No matching description
 }
-else
-{
-	header('Location: index.php');
-	print ' ';
+if (!empty($log_msg)) {
+	AddToLog("MSG>{$log_msg}; script terminated");
+	AddToLog("UA>{$ua}<");
+	AddToLog("URI>{$_SERVER["REQUEST_URI"]}<");
+	header('HTTP/1.1 403 Access Denied');
+	print "Hackers are not welcome here.";
 	exit;
 }
-switch($modinfo['Module']['type'])
-{
+switch($modinfo['Module']['type']) {
 	case PGV_MOD_SIMPLE:
-	{
-		if (!isset ($pgvaction))
-		{
-			$pgvaction = 'index';
+		if (isset($_REQUEST['pgvaction'])) $oldAction = $_REQUEST['pgvaction'];
+		else $oldAction = '';
+		if ($pgvaction != $oldAction && $oldAction != '') {
+			$log_msg = "Illegal pgvaction: {$oldAction}";
+			break;
 		}
-		if (!file_exists(PGV_ROOT.'modules/'.$mod.'/'.$pgvaction.'.php'))
-		{
-			$pgvaction = 'index';
+		if (file_exists(PGV_ROOT.'modules/'.$mod.'/'.$pgvaction.'.php')) require_once PGV_ROOT.'modules/'.$mod.'/'.$pgvaction.'.php';
+		else {
+			if ($oldAction == '') $log_msg = 'Missing pgvaction';
+			else $log_msg = "Unknown pgvaction: {$pgvaction}";
 		}
-		require_once PGV_ROOT.'modules/'.$mod.'/'.$pgvaction.'.php';
 		break;
-	}
 	case PGV_MOD_OO:
-	{
-		if (!isset ($method))
-		{
-			$method = 'main';
+		if (isset($_REQUEST['method'])) $oldMethod = $_REQUEST['method'];
+		else $oldMethod = '';
+		if ($method != $oldMethod && $oldMethod != '') {
+			$log_msg = "Illegal method: {$oldMethod}";
+			break;
 		}
-		if (!isset ($class))
-		{
-			$class = $mod;
+		if (isset($_REQUEST['class'])) $oldClass = $_REQUEST['class'];
+		else $oldClass = '';
+		if ($class != $oldClass && $oldClass != '') {
+			$log_msg = "Illegal class: {$oldClass}";
+			break;
 		}
-		require_once PGV_ROOT.'modules/'.$mod.'/'.$class.'.php';
+		if (file_exists(PGV_ROOT.'modules/'.$mod.'/'.$class.'.php')) {
+			require_once PGV_ROOT.'modules/'.$mod.'/'.$class.'.php';
+		} else {
+			if ($oldClass == '') $log_msg = "Missing class";
+			else $log_msg = "Unknown class: {$oldClass}";
+			break;
+		}
 		$mod = new $class();
-		if (!method_exists($mod, $method))
-		{
+		if (!method_exists($mod, $method)) {
 			$method = 'main';
 		}
 		$out = $mod->$method();
-		if (is_string($out))
-		{
+		if (is_string($out)) {
 			print $out;
 		}
 		break;
-	}
 	case PGV_MOD_V2:
-	{
 /*
  * Module Security
  *	1. Test if module is active.
  *	2. Only Admins can view an inactive module.
  */
-		if((!isset($modinfo['Config']['active']) || $modinfo['Config']['active'] === false) && !PGV_USER_IS_ADMIN)
-		{
-			header("Location: {$SERVER_URL}index.php");print ' ';exit;
+		if ((!isset($modinfo['Config']['active']) || $modinfo['Config']['active'] === false) && !PGV_USER_IS_ADMIN) {
+			header("Location: {$SERVER_URL}index.php");
+			print ' ';
+			exit;
 		}
 /*
  * Class Security
@@ -123,12 +131,17 @@ switch($modinfo['Module']['type'])
  *	2. Test if class file actually exists.
  *	3. Ignore any filename that starts with an underscore.
  */
-		if(isset($class)){$class = basename($class, '.php');}
-		if(
-			!isset($class) ||
-			!file_exists("modules/{$mod}/{$class}.php") ||
-			$class[0] == '_'
-		){$class = $mod;}
+		if (isset($_REQUEST['class'])) $oldClass = $_REQUEST['class'];
+		else $oldClass = '';
+		if ($class != $oldClass && $oldClass != '') {
+			$log_msg = "Illegal class: {$oldClass}";
+			break;
+		}
+		if (!file_exists("modules/{$mod}/{$class}.php") || $class[0] == '_') {
+			if ($oldClass == '') $log_msg = "Empty class";
+			else $log_msg = "Unknown class: {$oldClass}";
+			break;
+		}
 /*
  * Load Language
  *	1. Load english language if exists.
@@ -157,34 +170,32 @@ switch($modinfo['Module']['type'])
  */
 		require_once PGV_ROOT.'modules/'.$mod.'/'.$class.'.php';
 		$mod = new $class();
-		if(method_exists($mod, 'init')){$mod->init();}
+		if (method_exists($mod, 'init')) {
+			$mod->init();
+		}
 /*
  * Method Security
  *	1. Test if method actually exists in this object.
  *	2. Ignore any method that starts with an underscore.
  */
-		if(
-			!isset($method) ||
-			!method_exists($mod, $method) ||
-			$method[0] == '_'
-		){$method = 'main';}
+		if (!isset($method) || !method_exists($mod, $method) || $method[0] == '_') {
+			$method = 'main';
+		}
 /*
  * Execute Method
  *	1. Execute the requested method.
  *	2. Act upon the result of the method call.
  */
 		$results = $mod->$method();
-		switch($results[0])
+		switch ($results[0])
 		{
 /*
  * Action: Display Raw Output
  *	'content':	Raw content to display on the page.
  */
 			case 'display':
-			{
 				print $results['content'];
 				break;
-			}
 /*
  * Action: Wrap Output In Header & Footer
  *	'title'		Title of the page. [optional]
@@ -192,61 +203,63 @@ switch($modinfo['Module']['type'])
  *	'content'	Content to display on the page.
  */
 			case 'wrap':
-			{
-				if(!isset($results['title']))
-				{
-					if(isset($modinfo['Config']['title'])){$results['title'] = $modinfo['Config']['title'];}
-					else{$results['title'] = get_gedcom_setting(PGV_GED_ID, 'title');}
+				if (!isset($results['title'])) {
+					if (isset($modinfo['Config']['title'])) {
+						$results['title'] = $modinfo['Config']['title'];
+					} else {
+						$results['title'] = get_gedcom_setting(PGV_GED_ID, 'title');
+					}
 				}
-				if(!isset($results['head'])){$results['head'] = '';}
+				if (!isset($results['head'])) {
+					$results['head'] = '';
+				}
 				print_header($results['title'], $results['head']);
 				print $results['content'];
 				print_footer();
 				break;
-			}
 /*
  * Action: Redirect Browser
  *	'url'		URL to redirect the browser to.
  */
 			case 'redirect':
-			{
 				// fully qualified url is recomended.
-				if(!isFileExternal($results['url'])){$results['url'] = "{$SERVER_URL}{$results['url']}";}
+				if (!isFileExternal($results['url'])) {
+					$results['url'] = "{$SERVER_URL}{$results['url']}";
+				}
 				header("Location: ".encode_url($results['url'], false));
 				print ' '; // for some older browsers.
 				exit;
-			}
 /*
  * Action: Exit
  */
 			case 'exit':
-			{
 				exit;
-			}
 /*
  * Action: Error
  */
 			default:
-			{
 				print_header($results['title'], $results['head']);
 				print str_replace('[action]', $results['action'], $pgv_lang['module_error_unknown_action_v2']);
 				print_footer();
 				exit;
-			}
 		}
 		break;
-	}
 	default:
-	{
 		print_header(get_gedcom_setting(PGV_GED_ID, 'title'));
 		print $pgv_lang['module_error_unknown_type'];
 		print_footer();
 		break;
-	}
+}
+if (!empty($log_msg)) {
+	AddToLog("MSG>{$log_msg}; script terminated");
+	AddToLog("UA>{$ua}<");
+	AddToLog("URI>{$_SERVER["REQUEST_URI"]}<");
+	header('HTTP/1.1 403 Access Denied');
+	print "Hackers are not welcome here.";
+	exit;
 }
 
-function mod_print_header($title, $head='', $use_alternate_styles=true)
-{
+function mod_print_header($title, $head='', $use_alternate_styles=true) {
 	ob_start();
 	print_header($title, $head, $use_alternate_styles);
 	$out = ob_get_contents();
@@ -254,8 +267,7 @@ function mod_print_header($title, $head='', $use_alternate_styles=true)
 	return $out;
 }
 
-function mod_print_simple_header($title)
-{
+function mod_print_simple_header($title) {
 	ob_start();
 	print_simple_header($title);
 	$out = ob_get_contents();
@@ -263,8 +275,7 @@ function mod_print_simple_header($title)
 	return $out;
 }
 
-function mod_print_footer()
-{
+function mod_print_footer() {
 	ob_start();
 	print_footer();
 	$out = ob_get_contents();
@@ -272,8 +283,7 @@ function mod_print_footer()
 	return $out;
 }
 
-function mod_print_simple_footer()
-{
+function mod_print_simple_footer() {
 	ob_start();
 	print_simple_footer();
 	$out = ob_get_contents();
