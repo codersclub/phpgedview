293825959e8af8f9403c576838855f0d8853632e
canajun2eh <canajun2eh@8eba034c-2814-0410-b8e3-dd7cca3e1d4e>
2011-07-26 00:04:19 +0000

    Turkish language file updates

--- trunk/phpGedView/languages/countries.tr.php
+++ trunk/phpGedView/languages/countries.tr.php
@@ -3,7 +3,7 @@
  * Turkish Language file for PhpGedView.
  *
  * phpGedView: Genealogy Viewer
- * Copyright (C) 2002 to 2010  PGV Development Team.  All rights reserved.
+ * Copyright (C) 2002 to 2011  PGV Development Team.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,6 +23,45 @@
  * @author Kurt Norgaz
  * @author Adem GENÇ uzayuydu@gmail.com http://www.muttafi.com
  * @version $Id$
+ *
+ * see http://unstats.un.org/unsd/methods/m49/m49alpha.htm
+ * see http://www.foreignword.com/countries/  for a comprehensive list, with translations
+ * see http://susning.nu/Landskod  (list #7) for another list, taken from ISO standards
+ * see http://helpdesk.rootsweb.com/codes for a comprehensive list of Chapman codes.
+ * see http://www.rootsweb.com/~wlsgfhs/ChapmanCodes.htm for another list of Chapman codes
+ *
+ * The list that follows is the list of Chapman country codes, with additions from the
+ * other sources mentioned above.
+ *
+ * These codes do not appear in the two Chapman lists cited:
+ *		ALA		Åland Islands
+ *		CAT		Catalonia
+ *		COD		Congo (Brazzaville)		This country was known as Zaire
+ *		IOM		Isle of Man				Chapman lists this as part of the UK -- it's not!!
+ *		NFK		Norfolk Island
+ *		PRI		Puerto Rico				Chapman lists this as a state of the USA
+ *		SCG		Serbia and Montenegro	Chapman lists these separately
+ *		SSD		South Sudan				Newly (Jul 2011) independent from Sudan
+ *		TLS		Timor-Leste
+ *		UMI		US Minor Outlying Islands
+ *		VIR		US Virgin Islands		Chapman lists this as a state of the USA
+ *
+ * These Chapman country codes do not appear in the list following:
+ *		UEL		United Empire Loyalist		This is NOT a country or region, it's
+ *											a group of people
+ *		UK		United Kingdom				This is the only two-letter country code,
+ *											and GBR or one of its components should be
+ *											used instead.
+ *		SLK		Slovakia					This code, listed in the last source cited,
+ *											should be SVK
+ *		SLO		Slovenia					This code, listed in the last source cited,
+ *											should be SVN
+ *		SAM		South America				This code, listed in the last source cited,
+ *											is not precise enough
+ *		TMP		East Timor					Official name is TLS "Timor-Leste"
+ *		HOL		Holland						Official name is NLD "Netherlands"
+ *		ESM		Western Samoa				Official name is WSM "Samoa"
+ *
  */
 
 if (!defined('PGV_PHPGEDVIEW')) {
@@ -30,8 +69,9 @@ if (!defined('PGV_PHPGEDVIEW')) {
 	exit;
 }
 
-$countries["AFG"]="Afganistan";
 $countries["ABW"]="Aruba";
+$countries["ACA"]="Acadia";
+$countries["AFG"]="Afganistan";
 $countries["AGO"]="Angola";
 $countries["AIA"]="Anguilla";
 $countries["ALA"]="Oland Adaları";
@@ -48,6 +88,7 @@ $countries["ATG"]="Antigua ve Barbuda";
 $countries["AUS"]="Avustralya";
 $countries["AUT"]="Avusturya";
 $countries["AZE"]="Azerbaycan";
+$countries["AZR"]="Asor adalar";
 $countries["BDI"]="Burundi";
 $countries["BEL"]="Belçika";
 $countries["BEN"]="Benin";
@@ -71,7 +112,8 @@ $countries["BWI"]="İngiliz Bati Hint Adaları";
 $countries["CAF"]="Orta Afrika Cumhuriyeti";
 $countries["CAN"]="Kanada";
 $countries["CAP"]="Pelerin Sömürgesi";
-$countries["CCK"]="Cocos Adaları";
+$countries["CAT"]="Katalonya";
+$countries["CCK"]="Cocos (Keeling) Adaları";
 $countries["CHE"]="İsviçre";
 $countries["CHI"]="Kanal Adaları";
 $countries["CHL"]="Şili";
@@ -99,6 +141,7 @@ $countries["DOM"]="Dominik Cumhuriyeti";
 $countries["DZA"]="Cezayir";
 $countries["ECU"]="Ekvator";
 $countries["EGY"]="Mısır";
+$countries["EIR"]="Eire";
 $countries["ENG"]="İngiltere";
 $countries["ERI"]="Eritre";
 $countries["ESH"]="Batı Sahra";
@@ -107,6 +150,7 @@ $countries["EST"]="Estonya";
 $countries["ETH"]="Etiyopya";
 $countries["FIN"]="Finlandiya";
 $countries["FJI"]="Fiji";
+$countries["FLD"]="Flandra";
 $countries["FLK"]="Falkland Adaları";
 $countries["FRA"]="Fransa";
 $countries["FRO"]="Faroe Adaları";
@@ -136,6 +180,7 @@ $countries["HTI"]="Haiti";
 $countries["HUN"]="Macaristan";
 $countries["IDN"]="Endonezya";
 $countries["IND"]="Hindistan";
+$countries["IOM"]="Man Adası";
 $countries["IOT"]="İngiliz Hindistan Okyanus Toprakları";
 $countries["IRL"]="İrlanda";
 $countries["IRN"]="İran";
@@ -176,7 +221,7 @@ $countries["MHL"]="Marshall Adaları";
 $countries["MKD"]="Makedonya";
 $countries["MLI"]="Mali";
 $countries["MLT"]="Malta";
-$countries["MMR"]="Myanmar / Burma";
+$countries["MMR"]="Myanmar";
 $countries["MNG"]="Moğolistan";
 $countries["MNP"]="Mariyan Adaları";
 $countries["MNT"]="Karadağ";
@@ -240,7 +285,9 @@ $countries["SLV"]="El Salvador";
 $countries["SMR"]="San Marino";
 $countries["SOM"]="Somali";
 $countries["SPM"]="Saint Piyer ve Mikelon";
+$countries["SSD"]="Güney Sudan";
 $countries["STP"]="Sao Tome ve Prinsip";
+$countries["SUN"]="SSCB";
 $countries["SUR"]="Surinam";
 $countries["SVK"]="Slovak Cumhuriyeti";
 $countries["SVN"]="Slovenya";
@@ -255,8 +302,9 @@ $countries["THA"]="Tayland";
 $countries["TJK"]="Tacikistan";
 $countries["TKL"]="Tokelau";
 $countries["TKM"]="Türkmenistan";
-$countries["TLS"]="Timor-Leste Demokratik Cumhuriyeti";
+$countries["TLS"]="Timor-Leste";
 $countries["TON"]="Tonga";
+$countries["TRN"]="Transilvanya";
 $countries["TTO"]="Trinidad ve Tobago";
 $countries["TUN"]="Tunus";
 $countries["TUR"]="Türkiye";
@@ -267,7 +315,7 @@ $countries["UGA"]="Uganda";
 $countries["UKR"]="Ukrayna";
 $countries["UMI"]="ABD Minor Outlying Adaları";
 $countries["URY"]="Uruguay";
-$countries["USA"]="Amerika Birleşik Devletleri";
+$countries["USA"]="USA";
 $countries["UZB"]="Özbekistan";
 $countries["VAT"]="Vatikan";
 $countries["VCT"]="Saint Vincent ve Grenadinler";
@@ -283,17 +331,41 @@ $countries["WSM"]="Samoa";
 $countries["YEM"]="Yemen";
 $countries["YUG"]="Yugoslavya";
 $countries["ZAF"]="Güney Afrika";
+$countries["ZAR"]="Zaire";
 $countries["ZMB"]="Zambia";
 $countries["ZWE"]="Zimbabve";
 $countries["???"]="Bilinmeyen";
 
-$countries["EIR"]="Irlanda";
-$countries["AZR"]="Asor adalar";
-$countries["CAT"]="Katalonya";
-$countries["FLD"]="Flandra";
-$countries["SUN"]="SSCB";
-$countries["TRN"]="Transilvanya";
-$countries["ZAR"]="Zaire";
-$countries["IOM"]="Man Adası";
-$countries["ACA"]="Acadia";
+/*
+ * The following table lists alternate names for various Chapman codes.
+ * It will be used when country names have to be converted to Chapman codes.
+ * You do not have to list all the possibilities in all page languages.  This
+ * will be done automatically by the country-to-Chapman conversion routine.
+ *
+ * Because the list, and its contents, are specific to each language, the
+ * Translator Tool won't let you work on the list directly.  The list will
+ * have to be updated and amended manually.
+ *
+ * Suppose Chapman code "XYZ" represents the same country, and that country
+ * had the names "Name1", "Name2", "Name3" in its history.  It is now known
+ * as "Current name".  You can list the various names like this:
+ *
+ * $countries["XYZ"]="Current name";
+ * $altCountryNames["XYZ"]="Name1; Name2; Name3";
+ *
+ * The Chapman-to-country conversion will always use the $countries list of
+ * the current page language, no matter what the original country name was.
+ *
+ */
+$altCountryNames["DEU"]="ADC; Alman Demokratik Cumhuriyeti; FAC; Federal Almanya Cumhuriyeti";
+$altCountryNames["FLK"]="Malvinas";
+$altCountryNames["GBR"]="Büyük Britanya";
+$altCountryNames["LKA"]="Seylan";
+$altCountryNames["MAC"]="Macao";
+$altCountryNames["MMR"]="Burma";
+$altCountryNames["NLD"]="Hollanda";
+$altCountryNames["SUN"]="Sovyetler Birliği";
+$altCountryNames["USA"]="ABD; Amerika Birleşik Devletleri";
+$altCountryNames["WSM"]="Batı Samoa";
+
 ?>
